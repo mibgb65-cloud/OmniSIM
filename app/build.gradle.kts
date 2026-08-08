1af49a09837a760fe35a3dc8f60d34a84a4b4ddf
mimanchi412 <1915992742@qq.com>
2026-08-09 01:52:18 +0800

    Prepare OmniSIM 1.0.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "app.omnisim.android"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
